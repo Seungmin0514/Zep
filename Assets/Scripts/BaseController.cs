2ef56a2d33d8023b4e126308cd10d54fe491b3ae
Seungmin0514 <leessslee88@gmail.com>
2025-10-24 19:26:52 +0900

    feat Player Control/ add tilemap

---
+++ Assets/Scripts/BaseController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseController : MonoBehaviour
+{
+
+    protected Rigidbody2D _rigidbody;
+    [SerializeField] private SpriteRenderer PlayerRenderer;
+    [SerializeField] private Transform PokePivot;
+    [Range(1f, 20f)][SerializeField] private float speed = 3;
+    public float Speed
+    {
+        get => speed;
+        set => speed = Mathf.Clamp(value, 0, 20);
+    }
+    protected Vector2 movementDirection = Vector2.zero;
+    public Vector2 MovementDirection { get { return movementDirection; } }
+    protected Vector2 lookDirection = Vector2.zero;
+    public Vector2 LookDirection { get { return lookDirection; } }
+
+
+    protected virtual void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+
+
+    private void Movement(Vector2 direction)
+    {
+        direction = direction * Speed;
+        _rigidbody.velocity = direction;
+
+    }
+    protected virtual void FixedUpdate()
+    {
+        Movement(movementDirection);
+        
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : BaseController
+{
+    
+
+    void OnMove(InputValue inputValue)
+    {
+        movementDirection = inputValue.Get<Vector2>();
+        movementDirection = movementDirection.normalized;
+        
+    }
+}
